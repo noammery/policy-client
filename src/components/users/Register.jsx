e5afe2e60487a82dd5541fcda248a5331a725355
noammery <noammery55@gmail.com>
2022-09-21 17:52:17 +0300

    register

--- src/components/users/Register.jsx
+++ src/components/users/Register.jsx
@@ -1,86 +1,108 @@
-import React from "react";
 import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import React from "react";
+import "./register.css";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Link } from "react-router-dom";
 
-const schema = yup
-  .object({
-    firstName: yup
-      .string()
-      .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
-      .min(2, "Minimum 2 letters")
-      .max(40, "Max of 40 letters")
-      .required(),
-    lastName: yup
-      .string()
-      .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
-      .required(),
-    Email: yup.string().required().email("Email is not valid"),
-    MobileNumber: yup
-      .string()
-      .required()
-      .matches(/^[0-9]+$/, "Must be only digits")
-      .min(10, "Must be exactly 10 digits")
-      .max(10, "Must be exactly 10 digits"),
-    PassportID: yup
-      .string()
-      .required()
-      .matches(/^[0-9]+$/, "Must be only digits")
-      .min(9, "Must be exactly 9 digits")
-      .max(9, "Must be exactly 9 digits"),
-  })
-  .required();
+const schema = yup.object().shape({
+  firstName: yup
+    .string()
+    .required()
+    .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+  lastName: yup
+    .string()
+    .required()
+    .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+  phoneNumber: yup
+    .string()
+    .required()
+    .matches(/^[0-9]+$/, "Must be only digits")
+    .min(10, "Must be exactly 10 digits")
+    .max(10, "Must be exactly 10 digits"),
+  password: yup
+    .string()
+    .required()
+    .min(6, "Must be minimum 6 digits long")
+    .max(20, "Must be maximum 20 digits long"),
+  email: yup.string().email().required(),
+  passportID: yup
+    .string()
+    .required("passport ID is a required field")
+    .matches(/^[0-9]+$/, "Must be only digits")
+    .min(9, "Must be exactly 9 digits")
+    .max(9, "Must be exactly 9 digits"),
+  adress: yup.string().required(),
+});
 
 export default function Register() {
   const {
     register,
+    formState: { errors, isValid },
     handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+    reset,
+  } = useForm({ mode: "onBlur", resolver: yupResolver(schema) });
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    reset();
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="firstName"
-        placeholder="First name"
-        {...register("FirstName")}
-      />
-      <p>{errors.firstName?.message}</p>
-      <input
-        type="text"
-        placeholder="LastName"
-        {...register("Last name", { maxLength: 100 })}
-      />
-      <p>{errors.lastName?.message}</p>
-      <select {...register("Title")}>
-        <option value="Mr">Mr</option>
-        <option value="Mrs">Mrs</option>
-        <option value="Miss">Miss</option>
-        <option value="Dr">Dr</option>
-      </select>
-      <input
-        type="text"
-        placeholder="Email"
-        {...register("Email", { pattern: /^\S+@\S+$/i })}
-      />
-      <p>{errors.Email?.message}</p>
-      <input
-        type="tel"
-        placeholder="Mobile number"
-        {...register("MobileNumber", { maxLength: 12 })}
-      />
-      <p>{errors.MobileNumber?.message}</p>
-      <input
-        type="text"
-        placeholder="Passport ID"
-        {...register("PassportID", { max: 9, min: 9, maxLength: 9 })}
-      />
-      <p>{errors.PassportID?.message}</p>
-      <input type="submit" />
-    </form>
+    <div className="Register">
+      <h1>REGISTER</h1>
+      <h4>
+        If you already have an account 👉<Link to="/login">Log-in</Link>
+      </h4>
+
+      <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+        <div className="smallForm">
+          <label>First Name</label>
+          <input {...register("firstName")} />
+          {errors?.firstName && <p>{errors?.firstName?.message || "Error!"}</p>}
+        </div>
+        <div className="smallForm">
+          <label>Last Name</label>
+          <input {...register("lastName")} />
+          {errors?.lastName && <p>{errors?.lastName?.message || "Error!"}</p>}
+        </div>
+
+        <div className="smallForm">
+          <label>Phone Number </label>
+
+          <input {...register("phoneNumber")} placeholder="0505555555" />
+          {errors?.phoneNumber && (
+            <p>{errors?.phoneNumber?.message || "Error!"}</p>
+          )}
+        </div>
+
+        <div className="smallForm">
+          <label>Email Address</label>
+          <input {...register("email")} />
+          {errors?.email && <p>{errors?.email?.message || "Error!"}</p>}
+        </div>
+
+        <div className="smallForm">
+          <label>Passport ID</label>
+          <input {...register("passportID")} />
+          {errors?.passportID && (
+            <p>{errors?.passportID?.message || "Error!"}</p>
+          )}
+        </div>
+
+        <div className="smallForm">
+          <label>Billing Address</label>
+          <input {...register("adress")} />
+          {errors?.adress && <p>{errors?.adress?.message || "Error!"}</p>}
+        </div>
+        <div className="smallForm">
+          <label>Password</label>
+          <input type="password" {...register("password")} />
+          {errors?.password && <p>{errors?.password?.message || "Error!"}</p>}
+        </div>
+
+        <input value="SUBMIT" type="submit" disabled={!isValid} />
+      </form>
+    </div>
   );
 }
